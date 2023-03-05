86873787a145b1a41e7c6526d0ea14df202d698a
Rubel Hossasin <rubelrana019914@gmail.com>
2023-03-05 13:33:59 +0600

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const sgMail = require('@sendgrid/mail');
 app.use(
   cors({
-    origin: "https://michigansbestgolfdeals.com",
+    origin: "http://localhost:3000",
   })
 );
 app.use(cors());
