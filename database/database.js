d4f36cdf58879771602bf26ab4eb07c7e8ef25ca
Rubel Hossasin <rubelrana019914@gmail.com>
2023-03-09 03:13:52 +0600

    update

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const database = () => {
 
-      const uri = "mongodb+srv://rubelrana:ZYaNn4IhCJR2DdQR@cluster0.rm65m4m.mongodb.net/?retryWrites=true&w=majority"
+      const uri = "mongodb+srv://programmerrubel2018:VRLqJlZzHeWhdzlKrsss@cluster0.fiall5r.mongodb.net/?retryWrites=true&w=majority"
 
       mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
             .then((data) => {
