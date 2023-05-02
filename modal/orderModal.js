134116c52ad1ac547d18d0aaabacfea06ff07ae8
Rubel Hossasin <rubelrana019914@gmail.com>
2023-05-02 21:47:57 +0600

    update

--- modal/orderModal.js
+++ modal/orderModal.js
@@ -1,30 +1,28 @@
 const mongoose = require('mongoose');
 
-const orderShema = new mongoose.Schema({productId: {
-            type: mongoose.Schema.ObjectId,
-            ref: "Courses",
-            // required: true,
-          },
-          name: {
-              type: String ,
-              required: true,
-            },
-          email: {
-              type: String ,
-              required: true,
-            },
-            limit: {
-              type: Number,
-              // required: true,
-            },
-            
-            createdAt: {
-            type: Date,
-            default: Date.now,
-          },
-})
+const orderSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    limit: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
+const Order = mongoose.model('Order', orderSchema);
 
-const orderModal = new mongoose.model("Order", orderShema)
-
-module.exports = orderModal
+module.exports = Order;
