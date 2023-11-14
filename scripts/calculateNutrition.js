746559c556eca95f6c064bef6de7d381b9562a00
Rubel Hossain <rubelrana019914@gmail.com>
2023-11-14 21:00:24 +0600

    update

--- scripts/calculateNutrition.js
+++ scripts/calculateNutrition.js
@@ -114,17 +114,3 @@
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
-// {
-//     "builds": [
-//           {
-//                 "src": "index.js",
-//                 "use": "@vercel/node"
-//           }
-//     ],
-//     "routes": [
-//           {
-//                 "src": "/(.*)",
-//                 "dest": "index.js"
-//           }
-//     ]
-// }
